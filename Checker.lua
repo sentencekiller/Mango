ba6f2f830fc1cb925f1a3c3e1d3db1487062fefb
Isaac Sosa <isaacsosappss@gmail.com>
2024-05-26 19:56:38 -0700

    Create Checker.lua

---
+++ Checker.lua
@@ -0,0 +1,15 @@
+if game.PlaceId == 4399032158  then 
+loadstring(game:HttpGet("https://pastebin.com/raw/EnqBEkeV"))()
+  local Allowed = {
+  [16867953] = true,
+   [34024309] = true,
+     [34056244] = true
+  }
+  game.Players.PlayerAdded:Connect(function(player)
+    for idAllowed, _ in pairs(Allowed) do
+        if player:IsInGroup(idAllowed) then
+            player:Kick("Has sido expulsado del juego por pertenecer a un grupo prohibido.")
+        end
+    end
+end)
+end
